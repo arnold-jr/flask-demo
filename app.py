c1fe23656ca1f2ac1f6433b5b450a0192994b674
arnold-jr <arnold-jr@users.noreply.github.com>
2015-12-13 10:07:07 -0500

    About to clean templates

--- app.py
+++ app.py
@@ -1,63 +1,37 @@
 from flask import Flask, render_template, request, redirect
+from quandle_eg import make_plot
 
 app = Flask(__name__)
+    
+app.vars = {'ticker_name':'cmg',
+    'script':'',
+    'div':''}
 
-app.vars = {}
+flag,script,div = make_plot(app.vars['ticker_name'])
+if flag:
+  app.vars['script'] = script
+  app.vars['div'] = div
 
-app.questions={}
-app.questions['How many eyes do you have?']=('1','2','3')
-app.questions['Which fruit do you like best?']=('banana','mango','pineapple')
-app.questions['Do you like cupcakes?']=('yes','no','maybe')
-
-app.nquestions = len(app.questions)
-
-@app.route('/index_lulu',methods=['GET','POST'])
-def index_lulu():
+@app.route('/',methods=['GET','POST'])
+def main_redir():
+  return redirect('/index')
+      
+@app.route('/index',methods=['GET','POST'])
+def index():
   nquestions = 5
   if request.method == 'GET':
-    return render_template('userinfo_lulu.html',num=nquestions)
+    # pass the script and div to the boilerplate html
+    return render_template('boilerplate.html',
+        script=app.vars['script'],div=app.vars['div'])
   else:
     # request was a POST
-    app.vars['name'] = request.form['name_lulu']
-    app.vars['age'] = request.form['age_lulu']
-
-    f = open('%s_%s.txt'%(app.vars['name'],app.vars['age']),'w')
-    f.write('Name: %s\n'%(app.vars['name']))
-    f.write('Age: %s\n\n'%(app.vars['age']))
-    f.close()
-
-    return redirect('/main_lulu') 
-
-@app.route('/main_lulu',methods=['POST'])
-def main_lulu2():
-  if len(app.questions)==0:
-    return render_template('end_lulu.html')
-  return redirect('/next_lulu')
-
-@app.route('/next_lulu',methods=['GET'])
-def next_lulu():
-  n = app.nquestions - len(app.questions) + 1
-  q = app.questions.keys()[0]
-  a1, a2, a3 = app.questions.values()[0]
-
-  # save the current question key
-  app.currentq = q
-  
-  return render_template('layout_lulu.html',num=n,question=q,
-      ans1=a1,ans2=a2,ans3=a3)
-
-@app.route('/next_lulu',methods=['POST'])
-def next_lulu2():
-  f = open('%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age']),'a') #a is for append
-  f.write('%s\n'%(app_lulu.currentq))
-  f.write('%s\n\n'%(request.form['answer_from_layout_lulu'])) #this was the 'name' on layout.html!
-  f.close()
-
-  # Remove question from dictionary
-  del app.questions[app.currentq]
-
-  return redirect('/main_lulu')
-       
+    app.vars['ticker_name'] = request.form['ticker_name']
+    flag,script,div = make_plot(app.vars['ticker_name'])
+    if flag:
+      app.vars['script'] = script
+      app.vars['div'] = div
+    return render_template('boilerplate.html',
+        script=app.vars['script'],div=app.vars['div'])
 
 
 if __name__ == '__main__':
--- quandle_eg.py
+++ quandle_eg.py
@@ -7,68 +7,74 @@ import simplejson as json
 import pandas as pd
 import numpy as np
 from bokeh.plotting import figure, output_file, show
-
+from bokeh.embed import components
 
 def parse_ticker(ticker):
-    # the request is case insensitive
-    r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.json')
-    
-    # handle invalid tickers
-    if not 'dataset' in r.json():
-        return False, pd.DataFrame(), 'invalid'
-    else:
-        dict1 = r.json()['dataset']
-        col_names = dict1['column_names']
-        data = dict1['data']
-        name = dict1['name'][:-45]
-        df = pd.DataFrame(data,columns=col_names)
-        df['Date'] = pd.to_datetime(df['Date'])
-        df = df.loc[df['Date']>(df.loc[0,'Date']-np.timedelta64(31,'D'))]
-        return True, df, name
+  # the request is case insensitive
+  r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.json')
+  
+  # handle invalid tickers
+  if not 'dataset' in r.json():
+    return False, pd.DataFrame(), 'invalid'
+  else:
+    dict1 = r.json()['dataset']
+    col_names = dict1['column_names']
+    data = dict1['data']
+    name = dict1['name'][:-45]
+    df = pd.DataFrame(data,columns=col_names)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.loc[df['Date']>(df.loc[0,'Date']-np.timedelta64(31,'D'))]
+    return True, df, name
 
 
 def render_plot(df,name):
-    # output to static HTML file
-    output_file("lines.html", title="line plot example", mode='cdn')
+  # output to static HTML file
+  output_file("lines.html", title="line plot example")
 
-    TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
+  TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
 
-    # create a new plot with a title and axis labels
-    p = figure(width=1000, height=750,
-           title="simple line example", x_axis_type='datetime', 
-           x_axis_label='x', y_axis_label='y',
-           tools=TOOLS)
+  # create a new plot with a title and axis labels
+  p = figure(width=1000, height=750,
+         title="simple line example", x_axis_type='datetime', 
+         x_axis_label='x', y_axis_label='y',
+         tools=TOOLS)
 
-    # prepare some data
-    x = df['Date']
+  # prepare some data
+  x = df['Date']
 
-    # add a line renderer with legend and line thickness
-    p.line(x, df['Close'], legend="Close", line_width=1, color='gray')
-    p.inverted_triangle(x, df['High'], legend="High", line_width=1, color='black',
-                   size=10)
-    p.triangle(x, df['Low'], legend="Low", line_width=1, color='red',
-                   size=10)
+  # add a line renderer with legend and line thickness
+  p.line(x, df['Close'], legend="Close", line_width=1, color='gray')
+  p.inverted_triangle(x, df['High'], legend="High", line_width=1, color='black',
+                 size=10)
+  p.triangle(x, df['Low'], legend="Low", line_width=1, color='red',
+                 size=10)
 
-    # NEW: customize by setting attributes
-    p.title = name
-    #p.legend.orientation = "top_left"
-    p.grid.grid_line_alpha=0
-    p.xaxis.axis_label = 'Date'
-    p.xaxis.axis_label_text_font_size = '24pt'
-    p.xaxis.major_label_text_font_size = '20pt'
+  # NEW: customize by setting attributes
+  p.title = name
+  #p.legend.orientation = "top_left"
+  p.grid.grid_line_alpha=0
+  p.xaxis.axis_label = 'Date'
+  p.xaxis.axis_label_text_font_size = '24pt'
+  p.xaxis.major_label_text_font_size = '20pt'
 
-    p.yaxis.major_label_text_font_size = '20pt'
-    p.yaxis.axis_label = 'Price / $'
+  p.yaxis.major_label_text_font_size = '20pt'
+  p.yaxis.axis_label = 'Price / $'
 
-    p.yaxis.axis_label_text_font_size = '24pt'
-    p.ygrid.band_fill_color="olive"
-    p.ygrid.band_fill_alpha = 0.1
+  p.yaxis.axis_label_text_font_size = '24pt'
+  p.ygrid.band_fill_color="olive"
+  p.ygrid.band_fill_alpha = 0.1
 
-    # show the results
-    show(p)
+  # show the results
+  return components(p)
 
+def make_plot(ticker):
+  good_ticker, df, name = parse_ticker(ticker)
+  if good_ticker:
+    script, div = render_plot(df,name)
+    return True, script, div
+  else:
+    return False, '', ''
 
-good_ticker, df, name = parse_ticker('cmg')
-if good_ticker:
-    render_plot(df,name)
+if __name__=='__main__':
+  make_plot()
 
